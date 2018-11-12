4eead59514185daba99994e0ebb93280601eb035
jizampa <jizampa@gmail.com>
2018-11-12 12:43:52 -0600

    graph last 7 days working

---
+++ app/src/main/java/com/example/joao/myweatherapp/SplashScreen.java
@@ -0,0 +1,52 @@
+package com.example.joao.myweatherapp;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+    private ImageView logo;
+    private Animation myanim;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        
+
+        RssTask newRss = new RssTask();
+        newRss.execute();
+    }
+
+    class RssTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            Intent i = new Intent(SplashScreen.this,MainActivity.class);
+            startActivity(i);
+            finish();
+        }
+
+
+    }
+}
